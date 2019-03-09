c959f10ecd97f6be84dee7ce71df0f3bb027915a
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-09 14:25:38 -0300

    Add LoginController

---
+++ backend/src/main/java/br/com/taxidobarba/controller/LoginController.java
@@ -0,0 +1,28 @@
+package br.com.taxidobarba.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.taxidobarba.domain.dto.LoginRequestDTO;
+import br.com.taxidobarba.service.LoginService;
+
+@RestController
+public class LoginController {
+
+	@Autowired
+	LoginService service;
+
+	@PostMapping(value = "/api/v1/login")
+	public @ResponseBody ResponseEntity<HttpStatus> login(@RequestBody @Valid LoginRequestDTO request) {
+		service.login(request);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+
+}
