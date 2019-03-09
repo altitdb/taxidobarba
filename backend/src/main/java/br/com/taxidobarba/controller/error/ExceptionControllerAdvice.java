fc8c165581ddbf7c29440f13886340a9b6ceaf9d
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-09 14:27:14 -0300

    Add ErrorControllerAdvice

---
+++ backend/src/main/java/br/com/taxidobarba/controller/error/ExceptionControllerAdvice.java
@@ -0,0 +1,26 @@
+package br.com.taxidobarba.controller.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.taxidobarba.domain.dto.ErrorDTO;
+import br.com.taxidobarba.exception.AccessDeniedException;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ErrorDTO> accessDanied(final AccessDeniedException exception) {
+		ErrorDTO errorDTO = new ErrorDTO(exception.getMessage());
+		return new ResponseEntity<>(errorDTO, HttpStatus.FORBIDDEN);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDTO> genericException(final Exception exception) {
+		ErrorDTO errorDTO = new ErrorDTO(exception.getMessage());
+		return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+	}
+
+}
