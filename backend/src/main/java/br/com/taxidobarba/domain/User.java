5329d575b3e7b84b8977871e3c8ae25292039a44
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-09 14:27:55 -0300

    Add User

---
+++ backend/src/main/java/br/com/taxidobarba/domain/User.java
@@ -0,0 +1,45 @@
+package br.com.taxidobarba.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+
+	@Id
+	private String id;
+	private String username;
+	private String password;
+	private LocalDateTime lastAccess;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getLastAccess() {
+		return lastAccess;
+	}
+
+	public void setLastAccess(LocalDateTime lastAccess) {
+		this.lastAccess = lastAccess;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+}
