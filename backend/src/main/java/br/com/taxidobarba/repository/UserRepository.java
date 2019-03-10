7b1ba0696e6a46a1de1af4a2dc9517ed8556905b
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-10 00:38:36 -0300

    Adicionando anotacao @Repository

--- backend/src/main/java/br/com/taxidobarba/repository/UserRepository.java
+++ backend/src/main/java/br/com/taxidobarba/repository/UserRepository.java
@@ -3,9 +3,11 @@ package br.com.taxidobarba.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.taxidobarba.domain.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
 	public Optional<User> findByUsername(String username);
