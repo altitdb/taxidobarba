04886458339c148fb1ff6955348fb8cdd1e28679
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-11 01:16:05 -0300

    Create mocks

---
+++ backend/src/test/java/br/com/taxidobarba/mock/LoginRequestDTOMock.java
@@ -0,0 +1,25 @@
+package br.com.taxidobarba.mock;
+
+import java.lang.reflect.Field;
+
+import org.springframework.util.ReflectionUtils;
+
+import br.com.taxidobarba.domain.dto.LoginRequestDTO;
+
+public class LoginRequestDTOMock {
+
+	public static LoginRequestDTO mockLoginRequestDTO() {
+		LoginRequestDTO loginRequestDto = new LoginRequestDTO();
+
+		Field fieldUsername = ReflectionUtils.findField(LoginRequestDTO.class, "username");
+		Field fieldPassword = ReflectionUtils.findField(LoginRequestDTO.class, "password");
+
+		ReflectionUtils.makeAccessible(fieldUsername);
+		ReflectionUtils.makeAccessible(fieldPassword);
+
+		ReflectionUtils.setField(fieldUsername, loginRequestDto, "taxidobarba");
+		ReflectionUtils.setField(fieldPassword, loginRequestDto, "taxidobarba");
+
+		return loginRequestDto;
+	}
+}
