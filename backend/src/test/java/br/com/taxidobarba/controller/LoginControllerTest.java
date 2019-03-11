8679bb512f56e60e80f8251eed28c84e4c276290
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-11 01:13:36 -0300

    Extends contoller test generic

--- backend/src/test/java/br/com/taxidobarba/controller/LoginControllerTest.java
+++ backend/src/test/java/br/com/taxidobarba/controller/LoginControllerTest.java
@@ -3,54 +3,35 @@ package br.com.taxidobarba.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.lang.reflect.Field;
 import java.util.Optional;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.taxidobarba.domain.User;
 import br.com.taxidobarba.domain.dto.LoginRequestDTO;
+import br.com.taxidobarba.mock.LoginRequestDTOMock;
+import br.com.taxidobarba.mock.UserMock;
 import br.com.taxidobarba.repository.UserRepository;
 import br.com.taxidobarba.service.LoginServiceBean;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = { LoginController.class, LoginServiceBean.class })
-public class LoginControllerTest {
+public class LoginControllerTest extends ControllerTest{
 
 	@MockBean
 	private UserRepository repository;
-	@Autowired
-	private MockMvc mockMvc;
-	@Autowired
-	private ObjectMapper mapper;
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	@Before
-	public void setUp() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
-
+	private User mockUser = UserMock.mockUser();
+	private LoginRequestDTO loginRequestDto = LoginRequestDTOMock.mockLoginRequestDTO();
+	
 	@Test
 	public void shouldValidateValidUserAndPasswordHttpStatusAccepted() {
-
-		User mockUser = mockUser();
-		LoginRequestDTO loginRequestDto = mockLoginRequestDTO();
 
 		BDDMockito.given(repository.findByUsername(ArgumentMatchers.anyString())).willReturn(Optional.of(mockUser));
 
@@ -64,27 +45,5 @@ public class LoginControllerTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	private LoginRequestDTO mockLoginRequestDTO() {
-		LoginRequestDTO loginRequestDto = new LoginRequestDTO();
-
-		Field fieldUsername = ReflectionUtils.findField(LoginRequestDTO.class, "username");
-		Field fieldPassword = ReflectionUtils.findField(LoginRequestDTO.class, "password");
-
-		ReflectionUtils.makeAccessible(fieldUsername);
-		ReflectionUtils.makeAccessible(fieldPassword);
-
-		ReflectionUtils.setField(fieldUsername, loginRequestDto, "taxidobarba");
-		ReflectionUtils.setField(fieldPassword, loginRequestDto, "taxidobarba");
-
-		return loginRequestDto;
-	}
-
-	private User mockUser() {
-		User user = new User();
-		user.setUsername("taxidobarba");
-		user.setPassword("52007496FBC1E6093E711F0EBFD749A7957BDEFE44EE82DA80E2322848514FE3");
-		return user;
 	}
 }
