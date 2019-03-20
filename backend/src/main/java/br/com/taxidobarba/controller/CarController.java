5bebdaaa82e6c24d595f68a1160f4c9daa96c26b
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-19 23:16:41 -0300

    [#13] Add CarController findAll endpoint

--- backend/src/main/java/br/com/taxidobarba/controller/CarController.java
+++ backend/src/main/java/br/com/taxidobarba/controller/CarController.java
@@ -1,16 +1,20 @@
 package br.com.taxidobarba.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.taxidobarba.domain.dto.CarRequestDTO;
+import br.com.taxidobarba.domain.dto.CarResponseDTO;
 import br.com.taxidobarba.service.CarService;
 
 @RestController
@@ -23,6 +27,12 @@ public class CarController {
     public @ResponseBody ResponseEntity<HttpStatus> save(@RequestBody @Valid CarRequestDTO request) {
         service.save(request);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+    
+    @GetMapping(value = "/api/v1/car")
+    public @ResponseBody ResponseEntity<List<CarResponseDTO>> findAll(){
+        List<CarResponseDTO> carsResponseDto = service.findAll();
+        return new ResponseEntity<>(carsResponseDto,HttpStatus.OK);
     }
 
 }
