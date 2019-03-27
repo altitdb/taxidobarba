bd2b8c03299723983bbd170042b40fde42d65f58
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-26 23:25:37 -0300

    [#15] Add CashRegisterCityServices

---
+++ backend/src/main/java/br/com/taxidobarba/service/CashRegisterCityService.java
@@ -0,0 +1,9 @@
+package br.com.taxidobarba.service;
+
+import br.com.taxidobarba.domain.dto.CashRegisterCityRequestDTO;
+
+public interface CashRegisterCityService {
+
+    public void save(CashRegisterCityRequestDTO request);
+    
+}
