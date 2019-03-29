330e5e663b97c99e3538404ffe32670aa36d8c86
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-29 00:00:43 -0300

    [#16] Add toString, bean validation and renamed class

--- backend/src/main/java/br/com/taxidobarba/domain/dto/FuelRequestDTO.java
+++ backend/src/main/java/br/com/taxidobarba/domain/dto/FuelRequestDTO.java
@@ -3,13 +3,25 @@ package br.com.taxidobarba.domain.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class FuelResquestDTO {
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 
+public class FuelRequestDTO {
+
+    @NotNull
+    @DecimalMin(value = "0.01")
     private BigDecimal km;
+    @NotNull
+    @DecimalMin(value = "0.01")
     private BigDecimal liters;
+    @NotNull
+    @DecimalMin(value = "0.01")
     private BigDecimal price;
+    @NotNull
     private LocalDate date;
+    @NotNull
     private String carId;
+    @NotNull
     private String driverId;
 
     public BigDecimal getKm() {
@@ -34,6 +46,12 @@ public class FuelResquestDTO {
 
     public String getDriverId() {
         return driverId;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelResquestDTO [km=" + km + ", liters=" + liters + ", price=" + price + ", date=" + date + ", carId="
+                + carId + ", driverId=" + driverId + "]";
     }
 
 }
