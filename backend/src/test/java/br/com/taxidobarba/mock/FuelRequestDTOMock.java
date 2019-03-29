38e0837b0b052deb0780e8458b6ba4d10371a220
fernandoWPF <fernando.wesleypf@gmail.com>
2019-03-29 00:11:55 -0300

    [#16] Add mock request to fuel

---
+++ backend/src/test/java/br/com/taxidobarba/mock/FuelRequestDTOMock.java
@@ -0,0 +1,29 @@
+package br.com.taxidobarba.mock;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.taxidobarba.domain.dto.FuelRequestDTO;
+
+public class FuelRequestDTOMock extends GenericRequestDTOMock{
+
+    public static FuelRequestDTO mock() {
+
+        FuelRequestDTO fuelRequestDTO = new FuelRequestDTO();
+        
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("km", BigDecimal.ONE);
+        attributes.put("liters", BigDecimal.TEN);
+        attributes.put("price", BigDecimal.ONE);
+        attributes.put("date", LocalDate.now());
+        attributes.put("carId", "aptoy548erfd21");
+        attributes.put("driverId", "aptoy548erfd21");
+        
+        return createRequestDto(fuelRequestDTO, attributes);
+
+    }
+
+}
