62e112c0316a001f70b29b47f8a303b1f114d136
fernandoWPF <fernando.wesleypf@gmail.com>
2019-04-05 00:00:06 -0300

    [#29] Implemented @Query annotation for queries with between clausule

--- backend/src/main/java/br/com/taxidobarba/repository/CashRegisterCityRepository.java
+++ backend/src/main/java/br/com/taxidobarba/repository/CashRegisterCityRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.taxidobarba.domain.CashRegisterCity;
@@ -11,6 +12,7 @@ import br.com.taxidobarba.domain.CashRegisterCity;
 @Repository
 public interface CashRegisterCityRepository extends MongoRepository<CashRegisterCity, String> {
 
+    @Query(value = "{ 'date' : {$gte : ?0, $lte: ?1 }}")
     public List<CashRegisterCity> findByDateBetween(LocalDate initialDate, LocalDate finalDate);
 
 }
--- backend/src/main/java/br/com/taxidobarba/repository/FuelRepository.java
+++ backend/src/main/java/br/com/taxidobarba/repository/FuelRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.taxidobarba.domain.Fuel;
@@ -11,6 +12,7 @@ import br.com.taxidobarba.domain.Fuel;
 @Repository
 public interface FuelRepository extends MongoRepository<Fuel, String> {
 
+    @Query(value = "{ 'date' : {$gte : ?0, $lte: ?1 }}")
     public List<Fuel> findByDateBetween(LocalDate initialDate, LocalDate finalDate);
 
 }
