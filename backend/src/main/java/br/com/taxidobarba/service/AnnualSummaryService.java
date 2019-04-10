b941078df38548364fff2be17ef1ddcdc314d6b8
fernandoWPF <fernando.wesleypf@gmail.com>
2019-04-09 23:09:15 -0300

    [#31] Created AnnualSummaryService

---
+++ backend/src/main/java/br/com/taxidobarba/service/AnnualSummaryService.java
@@ -0,0 +1,9 @@
+package br.com.taxidobarba.service;
+
+import br.com.taxidobarba.business.results.annualsummary.AnnualSummaryDTO;
+
+public interface AnnualSummaryService {
+
+    public AnnualSummaryDTO generate();
+
+}
