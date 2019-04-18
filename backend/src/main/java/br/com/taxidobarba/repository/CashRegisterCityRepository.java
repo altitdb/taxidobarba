0b2ffd53fefa04f377f31cef31388b6eebfd582b
fernandoWPF <fernando.wesleypf@gmail.com>
2019-04-18 00:12:21 -0300

    [#15] Updated CashRegisterCityRepository

--- backend/src/main/java/br/com/taxidobarba/repository/CashRegisterCityRepository.java
+++ backend/src/main/java/br/com/taxidobarba/repository/CashRegisterCityRepository.java
@@ -2,17 +2,21 @@ package br.com.taxidobarba.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.taxidobarba.domain.CashRegisterCity;
+import br.com.taxidobarba.domain.Driver;
 
 @Repository
 public interface CashRegisterCityRepository extends MongoRepository<CashRegisterCity, String> {
 
     @Query(value = "{ 'date' : {$gte : ?0, $lte: ?1 }}")
     public List<CashRegisterCity> findByDateBetween(LocalDate initialDate, LocalDate finalDate);
+    
+    public Optional<CashRegisterCity> findByDriverAndDate(Driver driver, LocalDate date);
 
 }
