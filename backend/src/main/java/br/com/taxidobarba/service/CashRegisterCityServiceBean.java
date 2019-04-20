894c80009443b60958dad6415aa231af60b85b0a
fernandoWPF <fernando.wesleypf@gmail.com>
2019-04-20 02:30:12 -0300

    [#39] Implementation update CashRegisterCity

--- backend/src/main/java/br/com/taxidobarba/service/CashRegisterCityServiceBean.java
+++ backend/src/main/java/br/com/taxidobarba/service/CashRegisterCityServiceBean.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.taxidobarba.domain.Car;
 import br.com.taxidobarba.domain.CashRegisterCity;
+import br.com.taxidobarba.domain.CashRegisterCity.CashRegisterCityBuilder;
 import br.com.taxidobarba.domain.Driver;
 import br.com.taxidobarba.domain.dto.CarCashRegisterDTO;
 import br.com.taxidobarba.domain.dto.CashRegisterCityRequestDTO;
@@ -36,17 +37,74 @@ public class CashRegisterCityServiceBean implements CashRegisterCityService {
     @Override
     public CashRegisterCityResponseDTO save(CashRegisterCityRequestDTO request) {
         LOG.info("Dados recebidos no request: {}", request);
-        validateRequest(request);
-        CashRegisterCity cashRegisterCity = cashRegisterCityRequestDtoToEntity(request);
+        
+        validateRequest(request, false);
+        CashRegisterCity cashRegisterCity = requestDtoToEntity(request);
+        
         LOG.info("Persistindo cashCity...");
-        CashRegisterCity city = cashCityRepository.save(cashRegisterCity);
+        cashCityRepository.save(cashRegisterCity);
         LOG.info("Dados persistidos.");
         
-        return entityToResponseDto(city);
-        
+        return entityToResponseDto(cashRegisterCity);
     }
 
-    private CashRegisterCity cashRegisterCityRequestDtoToEntity(CashRegisterCityRequestDTO request) {
+    @Override
+    public CashRegisterCityResponseDTO update(String id, CashRegisterCityRequestDTO request) {
+        LOG.info("Dados recebidos no request: {}", request);
+        
+        validateRequest(request, true);
+        CashRegisterCity cashRegisterCity = findCashRegisterCityById(id);
+        requestDtoToEntity(cashRegisterCity, request);
+        
+        LOG.info("Validando dados alterados...");
+        String driver = cashRegisterCity.getDriver().getId();
+        LocalDate date = cashRegisterCity.getDate();
+        String cashRegisterId = cashRegisterCity.getId();
+        validateRegisterByDateAndDriverAndId(driver, date, cashRegisterId);
+        LOG.info("Dados validados.");
+        
+        cashCityRepository.save(cashRegisterCity);
+        
+        return entityToResponseDto(cashRegisterCity);
+    }
+    
+    /**
+     * Request validator
+     * @param request requisition to be validated
+     * @param  isUpdate true if change, false if new record
+    */
+    private void validateRequest(CashRegisterCityRequestDTO request, boolean isUpdate) {
+        validateKm(request.getStartKm(), request.getEndKm());
+        LOG.debug("isUpdate? {}", isUpdate);
+        if (!isUpdate) {
+            validateRegisterByDateAndDriver(request.getDriver(), request.getDate());
+        }
+    }
+    
+    private void validateKm(BigDecimal startKm, BigDecimal endKm) {
+        if (startKm.compareTo(endKm) > 0)
+            throw new BusinessException("KM inicial nao pode ser maior que KM final.");
+    }
+    
+    private void validateRegisterByDateAndDriver(String driverId, LocalDate date) {
+        Driver driver = findDriverById(driverId);
+        cashCityRepository.findByDriverAndDate(driver, date).ifPresent(city -> {
+            throw new BusinessException("Ja existe um registro para essa data e motorista");
+        });
+    }
+    
+    private void validateRegisterByDateAndDriverAndId(String driverId, LocalDate date, String cashRegisterId) {
+        Driver driver = findDriverById(driverId);
+        cashCityRepository.findByDriverAndDateAndIdNot(driver, date, cashRegisterId).ifPresent(city -> {
+            throw new BusinessException("Ja existe um registro para essa data e motorista");
+        });
+    }
+    
+    private CashRegisterCity requestDtoToEntity(CashRegisterCityRequestDTO request) {
+        return requestDtoToEntity(null, request);
+    }
+    
+    private CashRegisterCity requestDtoToEntity(CashRegisterCity cashRegisterCity, CashRegisterCityRequestDTO request) {
         Car car = findCarById(request.getCar());
         Driver driver = findDriverById(request.getDriver());
 
@@ -59,19 +117,24 @@ public class CashRegisterCityServiceBean implements CashRegisterCityService {
         BigDecimal totalReceived = request.getTotalReceived();
         BigDecimal difference = calculateDifference(totalReceived, price);
         
-        return new CashRegisterCity.CashRegisterCityBuilder()
-                                .withCar(car)
-                                .withPrice(price)
-                                .withPriceKmDriver(priceKmDriver)
-                                .withDate(request.getDate())
-                                .withDriver(driver)
-                                .withEndKm(endKm)
-                                .withOtherKm(otherKm)
-                                .withStartKm(startKm)
-                                .withKm(km)
-                                .withTotalReceived(totalReceived)
-                                .withDifference(difference)
-                                .build();
+        CashRegisterCityBuilder builder = new CashRegisterCityBuilder();
+        
+        if(cashRegisterCity != null) {
+            builder = builder.forUpdate(cashRegisterCity);
+        }
+        
+        return builder.withCar(car)
+                      .withPrice(price)
+                      .withPriceKmDriver(priceKmDriver)
+                      .withDate(request.getDate())
+                      .withDriver(driver)
+                      .withEndKm(endKm)
+                      .withOtherKm(otherKm)
+                      .withStartKm(startKm)
+                      .withKm(km)
+                      .withTotalReceived(totalReceived)
+                      .withDifference(difference)
+                      .build();
     }
     
     private CashRegisterCityResponseDTO entityToResponseDto(CashRegisterCity city) {
@@ -106,22 +169,6 @@ public class CashRegisterCityServiceBean implements CashRegisterCityService {
                     .build();
     }
     
-    private void validateRequest(CashRegisterCityRequestDTO request) {
-        validateKm(request.getStartKm(), request.getEndKm());
-        validateRegisterByDateAndDriver(request.getDriver(), request.getDate());
-    }
-    
-    private void validateKm(BigDecimal startKm, BigDecimal endKm) {
-        if (startKm.compareTo(endKm) > 0)
-            throw new BusinessException("KM inicial nao pode ser maior que KM final.");
-    }
-    
-    private void validateRegisterByDateAndDriver(String driverId, LocalDate date) {
-        Driver driver = findDriverById(driverId);
-        cashCityRepository.findByDriverAndDate(driver, date).ifPresent(city -> {
-            throw new BusinessException("Ja existe um registro para essa data e motorista");
-        });
-    }
     
     private Car findCarById(String id) {
         LOG.info("Buscando carro...");
@@ -131,6 +178,11 @@ public class CashRegisterCityServiceBean implements CashRegisterCityService {
     private Driver findDriverById(String id) {
         LOG.info("Buscando motorista...");
         return driverRepository.findById(id).orElseThrow(() -> new BusinessException("Motorista nao localizado."));
+    }
+    
+    private CashRegisterCity findCashRegisterCityById(String id) {
+        return cashCityRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("CashRegisterCity nao localizado."));
     }
     
     private BigDecimal calculatePrice(BigDecimal endKm, BigDecimal startKm, BigDecimal otherKm,
@@ -145,4 +197,5 @@ public class CashRegisterCityServiceBean implements CashRegisterCityService {
     private BigDecimal calculateDifference(BigDecimal totalReceived, BigDecimal price) {
         return totalReceived.subtract(price);
     }
+
 }
