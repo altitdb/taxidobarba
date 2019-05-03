576e0834c2efaafbefdafa7446641b38880de0a0
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-02 23:41:18 -0300

    [#33] Adding Requisition Parameters

--- backend/src/main/java/br/com/taxidobarba/service/MonthlyReportService.java
+++ backend/src/main/java/br/com/taxidobarba/service/MonthlyReportService.java
@@ -4,6 +4,6 @@ import br.com.taxidobarba.business.results.monthlyreport.MonthlyReportDTO;
 
 public interface MonthlyReportService {
 
-    public MonthlyReportDTO generate();
+    public MonthlyReportDTO generate(Integer monthy, Integer year);
     
 }
