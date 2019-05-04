480cab236a2b80c51bf588590821119943a4aed2
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-04 01:48:49 -0300

    [#47] Created controller for TrafficTicket

---
+++ backend/src/main/java/br/com/taxidobarba/controller/TrafficTicketController.java
@@ -0,0 +1,28 @@
+package br.com.taxidobarba.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.taxidobarba.domain.dto.TrafficTicketRequestDTO;
+import br.com.taxidobarba.service.TrafficTicketService;
+
+@RestController
+public class TrafficTicketController {
+
+    @Autowired
+    private TrafficTicketService service;
+
+    @PostMapping(value = "/api/v1/traffic-ticket")
+    public @ResponseBody ResponseEntity<HttpStatus> save(@RequestBody @Valid TrafficTicketRequestDTO request) {
+        service.save(request);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+}
