379318c52450ef14027eafcd65a0d8a5eb09800d
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-04 02:05:54 -0300

    [#47] Created tests and mocks

---
+++ backend/src/test/java/br/com/taxidobarba/mock/TrafficTicketRequestDTOMock.java
@@ -0,0 +1,40 @@
+package br.com.taxidobarba.mock;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.taxidobarba.domain.dto.TrafficTicketRequestDTO;
+
+public class TrafficTicketRequestDTOMock extends GenericRequestDTOMock {
+
+    public static TrafficTicketRequestDTO mockFail() {
+        TrafficTicketRequestDTO trafficTicketRequestDto = new TrafficTicketRequestDTO();
+
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("car", "asct9547dg41");
+        attributes.put("driver", "asct9547dg41");
+        attributes.put("value", new BigDecimal(25));
+        attributes.put("points", 10);
+        attributes.put("dueDate", LocalDate.now());
+
+        return createRequestDto(trafficTicketRequestDto, attributes);
+    }
+    
+    public static TrafficTicketRequestDTO mockSuccess() {
+        TrafficTicketRequestDTO trafficTicketRequestDto = new TrafficTicketRequestDTO();
+
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("car", "asct9547dg41");
+        attributes.put("driver", "asct9547dg41");
+        attributes.put("value", new BigDecimal(25));
+        attributes.put("points", 5);
+        attributes.put("dueDate", LocalDate.now());
+
+        return createRequestDto(trafficTicketRequestDto, attributes);
+    }
+
+}
