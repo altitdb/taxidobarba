eb1d65f9a55dc5e27a25141def9ea19f867a4ef4
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-05 17:14:59 -0300

    [#48] Created domain PaymentTrafficTicket

---
+++ backend/src/main/java/br/com/taxidobarba/domain/PaymentTrafficTicket.java
@@ -0,0 +1,62 @@
+package br.com.taxidobarba.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "paymentTrafficTicket")
+public class PaymentTrafficTicket {
+
+    @Id
+    private String id;
+    private TrafficTicket trafficTicket;
+    private BigDecimal value;
+    private LocalDate date;
+
+    public static class PaymentTrafficTicketBuilder {
+
+        private PaymentTrafficTicket payment;
+
+        public PaymentTrafficTicketBuilder() {
+            payment = new PaymentTrafficTicket();
+        }
+
+        public PaymentTrafficTicketBuilder withTrafficTicket(TrafficTicket trafficTicket) {
+            payment.trafficTicket = trafficTicket;
+            return this;
+        }
+
+        public PaymentTrafficTicketBuilder withValue(BigDecimal value) {
+            payment.value = value;
+            return this;
+        }
+
+        public PaymentTrafficTicketBuilder withDate(LocalDate date) {
+            payment.date = date;
+            return this;
+        }
+
+        public PaymentTrafficTicket build() {
+            return payment;
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TrafficTicket getTrafficTicket() {
+        return trafficTicket;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+}
