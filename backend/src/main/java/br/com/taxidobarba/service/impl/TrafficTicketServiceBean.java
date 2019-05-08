ce9fbcf5224f2f75ec61a891d7757c9a608545ff
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-07 23:32:14 -0300

    [#49] Created query to return TrafficTicketResponseDTO

--- backend/src/main/java/br/com/taxidobarba/service/impl/TrafficTicketServiceBean.java
+++ backend/src/main/java/br/com/taxidobarba/service/impl/TrafficTicketServiceBean.java
@@ -1,6 +1,11 @@
-package br.com.taxidobarba.service;
+package br.com.taxidobarba.service.impl;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,12 +14,20 @@ import org.springframework.stereotype.Service;
 
 import br.com.taxidobarba.domain.Car;
 import br.com.taxidobarba.domain.Driver;
+import br.com.taxidobarba.domain.PaymentTrafficTicket;
 import br.com.taxidobarba.domain.TrafficTicket;
+import br.com.taxidobarba.domain.enumerated.TrafficTicketStatus;
 import br.com.taxidobarba.domain.request.dto.TrafficTicketRequestDTO;
+import br.com.taxidobarba.domain.response.dto.SimpleCarResponseDTO;
+import br.com.taxidobarba.domain.response.dto.SimpleDriverResponseDTO;
+import br.com.taxidobarba.domain.response.dto.TrafficTicketResponseDTO;
+import br.com.taxidobarba.domain.response.dto.TrafficTicketResponseDTO.TrafficTicketResponseBuilder;
 import br.com.taxidobarba.exception.BusinessException;
 import br.com.taxidobarba.repository.CarRepository;
 import br.com.taxidobarba.repository.DriverRepository;
+import br.com.taxidobarba.repository.PaymentTrafficTicketRepository;
 import br.com.taxidobarba.repository.TrafficTicketRepository;
+import br.com.taxidobarba.service.spec.TrafficTicketService;
 
 @Service
 public class TrafficTicketServiceBean implements TrafficTicketService {
@@ -27,6 +40,8 @@ public class TrafficTicketServiceBean implements TrafficTicketService {
     private DriverRepository driverRepository;
     @Autowired
     private TrafficTicketRepository trafficTicketRepository;
+    @Autowired
+    private PaymentTrafficTicketRepository paymentTrafficTicketRepository;
 
     @Override
     public void save(TrafficTicketRequestDTO request) {
@@ -37,6 +52,59 @@ public class TrafficTicketServiceBean implements TrafficTicketService {
         LOG.info("Persistindo dados...");
         trafficTicketRepository.save(requestToEntity(request));
         LOG.info("Dados persistidos.");
+    }
+
+    @Override
+    public List<TrafficTicketResponseDTO> findAll(TrafficTicketStatus status) {
+        List<TrafficTicket> allTrafficTickets = trafficTicketRepository.findAll();
+        List<TrafficTicketResponseDTO> trafficTicketResponseDtos = entityToResponseDto(allTrafficTickets);
+        
+        if(status != null) {
+            LOG.info("Buscando multas pelo status {}", status);
+            return trafficTicketResponseDtos.stream()
+                            .filter(dto -> dto.getStatus().equals(status))
+                            .collect(Collectors.toList());
+        }
+        
+        LOG.info("Status não informado, retornando todos os registros...");
+        
+        return trafficTicketResponseDtos;
+    }
+    
+    private List<TrafficTicketResponseDTO> entityToResponseDto(List<TrafficTicket> allTrafficTickets) {
+        
+        List<PaymentTrafficTicket> payments = paymentTrafficTicketRepository.findAll();
+        List<TrafficTicketResponseDTO> trafficTicketResponseDtos = new ArrayList<>();
+        
+        allTrafficTickets.stream()
+                .forEach(ticket -> {
+                    BigDecimal amountPaid = 
+                            payments.stream()
+                                .filter(payment -> payment.getTrafficTicket().getId().equals(ticket.getId()))
+                                .map(PaymentTrafficTicket::getValue)
+                                .reduce(BigDecimal.ZERO, BigDecimal::add);
+                    
+                    TrafficTicketResponseBuilder builder = new TrafficTicketResponseBuilder();
+                    
+                    builder.withCar(new SimpleCarResponseDTO(ticket.getCar()))
+                           .withDriver(new SimpleDriverResponseDTO(ticket.getDriver()))
+                           .withValue(ticket.getValue())
+                           .withPaidValue(amountPaid)
+                           .withDueDate(ticket.getDueDate());
+                    
+                    if(amountPaid.compareTo(ticket.getValue()) == 0) {
+                        builder.withStatus(TrafficTicketStatus.PAID);
+                    }else {
+                        builder.withStatus(TrafficTicketStatus.OPENED);
+                    }
+                    
+                    trafficTicketResponseDtos.add(builder.build());
+                    
+                });
+        
+        trafficTicketResponseDtos.sort(Comparator.comparing(TrafficTicketResponseDTO::getDueDate)); 
+        
+        return trafficTicketResponseDtos;
     }
 
     private void validateRequest(TrafficTicketRequestDTO request) {
@@ -60,9 +128,6 @@ public class TrafficTicketServiceBean implements TrafficTicketService {
                             .withValue(request.getValue())
                             .build();
     }
-
-
-
 
     private Car findCarById(String id) {
         LOG.info("Buscando carro...");
--- backend/src/main/java/br/com/taxidobarba/service/spec/TrafficTicketService.java
+++ backend/src/main/java/br/com/taxidobarba/service/spec/TrafficTicketService.java
@@ -1,9 +1,15 @@
-package br.com.taxidobarba.service;
+package br.com.taxidobarba.service.spec;
 
+import java.util.List;
+
+import br.com.taxidobarba.domain.enumerated.TrafficTicketStatus;
 import br.com.taxidobarba.domain.request.dto.TrafficTicketRequestDTO;
+import br.com.taxidobarba.domain.response.dto.TrafficTicketResponseDTO;
 
 public interface TrafficTicketService {
 
     public void save(TrafficTicketRequestDTO request);
+    
+    public List<TrafficTicketResponseDTO> findAll(TrafficTicketStatus status);
     
 }
