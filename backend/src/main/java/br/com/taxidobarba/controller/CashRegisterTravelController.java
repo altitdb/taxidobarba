4c14f3f298382f4b203f4d445779cd7e0a1e2ba1
fernandoWPF <fernando.wesleypf@gmail.com>
2019-05-19 13:44:47 -0300

    [#58] Add findById to Travel

--- backend/src/main/java/br/com/taxidobarba/controller/CashRegisterTravelController.java
+++ backend/src/main/java/br/com/taxidobarba/controller/CashRegisterTravelController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,6 +33,11 @@ public class CashRegisterTravelController {
     public @ResponseBody ResponseEntity<CashRegisterTravelResponseDTO> update(@PathVariable("id") String id,
             @RequestBody @Valid CashRegisterTravelRequestDTO request) {
         return new ResponseEntity<>(service.update(id, request), HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "/api/v1/cash/travel/{id}")
+    public @ResponseBody ResponseEntity<CashRegisterTravelResponseDTO> find(@PathVariable String id){
+        return new ResponseEntity<>(service.find(id), HttpStatus.OK);
     }
 
 }
